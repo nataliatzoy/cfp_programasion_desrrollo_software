15f560c54348d9143a9584dee9fdd0bababc1e87
Ana Nathalí Tzoy Osorio <anatzoy@CFPBARTOLOMEAMBROSIO.COM>
2024-09-03 08:49:13 -0600

    se arreglan ejercicios js

--- modulo_dos/java/ejercicio_14.js
+++ modulo_dos/java/ejercicio_14.js
@@ -1,20 +1,20 @@
 class Vec {
-    constructor(x, y) {
-      this.x = x;
-      this.y = y;
-    }
-  
-    plus(otra) {
-      return new Vec(this.x + otra.x, this.y + otra.y);
-    }
-  
-    minus(otra) {
-      return new Vec(this.x - otra.x, this.y - otra.y);
-    }
-  
-    get length() {
-      return Math.sqrt(this.x * this.x + this.y * this.y);
-    }
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  plus(otra) {
+    return new Vec(this.x + otra.x, this.y + otra.y);
+  }
+
+  minus(otra) {
+    return new Vec(this.x - otra.x, this.y - otra.y);
+  }
+
+  get length() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
 }
   
 console.log(new Vec(9, 2).plus(new Vec(2, 7)));
