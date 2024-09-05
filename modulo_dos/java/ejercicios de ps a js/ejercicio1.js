c363e3b1261b2de9bb061dd13cc45c8c481e807b
Ana Nathalí Tzoy Osorio <anatzoy@CFPBARTOLOMEAMBROSIO.COM>
2024-09-05 15:23:44 -0600

    se pasan ejercicios de ps a js

---
+++ modulo_dos/java/ejercicios de ps a js/ejercicio1.js
@@ -0,0 +1,13 @@
+let num1, num2, suma, resta, multiplicacion, divicion;
+ num1 = 5;
+ num2 = 5;
+
+ suma = num1 + num2;
+ resta = num1 - num2;
+ multiplicacion= num1*num2;
+ divicion= num1/ num2;
+ 
+ console.log(suma);
+ console.log(resta);
+ console.log(multiplicacion);
+ console.log(divicion);
---
+++ modulo_dos/java/ejercicios de ps a js/ejercicio2.js
@@ -0,0 +1,10 @@
+let area, perimetro, base, altura;
+
+base=5;
+altura=8;
+
+area= base*altura;
+perimetro= (base+ altura)*2;
+
+console.log('el area es de:',area);
+console.log('el perimetro es de:', perimetro);
