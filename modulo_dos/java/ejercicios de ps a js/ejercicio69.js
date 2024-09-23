636fb6bd82584e470b776bd742fb3d024cb8d370
Ana Nathalí Tzoy Osorio <anatzoy@CFPBARTOLOMEAMBROSIO.COM>
2024-09-23 14:20:40 -0600

    se suben cambios

---
+++ modulo_dos/java/ejercicios de ps a js/ejercicio69.js
@@ -0,0 +1,17 @@
+function primo(numero) {
+    if (numero <= 1) return false;
+    if (numero <= 3) return true;
+
+    if (numero % 2 === 0 || numero % 3 === 0) return false;
+
+    for (let i = 5; i * i <= numero; i += 6) {
+        if (numero % i === 0 || numero % (i + 2) === 0) return false;
+    }
+
+    return true;
+}
+
+
+console.log('el numero no es primo ',primo(10));
+
+console.log('el numero es primo', primo(7));
---
+++ modulo_dos/java/ejercicios de ps a js/ejercicio70.js
@@ -0,0 +1,7 @@
+function fahre(valor1){
+    return valor1 *(9/5) +32
+}
+
+resultado = fahre(9)
+
+console.log(resultado);
