608036cc93a193ed9a1ec08fc74c2df22f1a4ff9
Ana Nathalí Tzoy Osorio <anatzoy@CFPBARTOLOMEAMBROSIO.COM>
2024-10-10 15:41:11 -0600

    se actualiza

---
+++ modulo_dos/npm/ejercicio/ejercicio.js
@@ -0,0 +1,5 @@
+const express = requiere('express');
+const app = express();
+const port = 4000
+
+app.get ('/pokemon')
--- modulo_dos/npm/ejercicio/pokemon.js
+++ modulo_dos/npm/ejercicio/pokemon.js
@@ -3,4 +3,10 @@ const app = express()
 const port = 4000
 
 
- const poke =
+ const poke = [
+    {image: '', id: '0001', name: 'Bulbasaur', power: "Plant · Poison" },
+    {image: '', id: '0002', name: 'Ivysaur', power: 'Plant . Poison'},
+    {image: '', id: '0003', name: 'Venusaur', power: 'Plant . Poison'},
+    {image: '', id: '0004', name: 'Charmander', power: 'Fire'}
+ ];
+ 
