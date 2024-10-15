11993364bbd8c82ce05e4838166c32b4f617f171
Ana Nathalí Tzoy Osorio <anatzoy@CFPBARTOLOMEAMBROSIO.COM>
2024-10-15 14:12:14 -0600

    se realizan cambios

--- modulo_dos/npm/ejercicio/ejercicio.js
+++ modulo_dos/npm/ejercicio/ejercicio.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8888;
 const poke = [
-  {  image: "",id: 1,  name: 'Bulbasaur', power: "Grass · Poison" },
-  {  image: "",id: 2,  name: 'Ivysaur', power: "Grass · Poison" },
-  {  image: "",id: 3,  name: 'Venusaur', power: "Grass · Poison" },
+  {  image: "",id: 1,  name: 'Bulbasaur', power: "Plant · Poison" },
+  {  image: "",id: 2,  name: 'Ivysaur', power: "Plant · Poison" },
+  {  image: "",id: 3,  name: 'Venusaur', power: "Plant · Poison" },
   {  image: "",id: 4,  name: 'Charmander', power: "Fire" }
 ];
 
@@ -16,11 +16,11 @@ app.get('/', (req, res) => {
 app.get('/pokemon/:id', (req, res) => {
   
   const id = parseInt(req.params.id);
-  const product = poke.find(p => p.id === id);
+  const Pokes = poke.find(p => p.id === id);
 
 
-  if (product) {
-    res.json(product);
+  if (Pokes) {
+    res.json(Pokes);
   } else {
     res.status(404).send('Pokémon no encontrado');
   }
